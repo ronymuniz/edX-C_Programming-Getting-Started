328494cfe32bbcf9413351288a729bee08a8e2b0
decoder <ronymuniz@tutamail.com>
2020-01-15 01:02:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Declarações globais: bibliotecas usadas pelo programa
+
+int main(void) { // Innício do programa
+  printf("Hello World\n"); // Escreve uma mensagem na tela de saída
+  return 0; // Retorna o código de erro ZERO para o S.O informando que está tudo OK.
+} // Fim do programa.
