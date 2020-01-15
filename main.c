4cb4b216fa1d937b6b940752fb8119de6943a8a0
decoder <ronymuniz@tutamail.com>
2020-01-15 02:33:23 +0000

    sintaxe

--- main.c
+++ main.c
@@ -3,4 +3,4 @@
 int main(void) { // Início do programa
   printf("Hello World\n"); // Escreve uma mensagem na tela de saída
   return 0; // Retorna o código de erro ZERO para o S.O informando que está tudo OK.
-} // Fim do programa
+} // Fim do programa.
